e5eb5efe308458b7f8816fbd1d33b31885c9b148
Yakup <yakupjoraev@gmail.com>
2022-09-17 13:55:19 +0300

    added files

---
+++ src/js/main.js
@@ -0,0 +1,116 @@
+// Custom scripts
+
+
+// Мобильное меню бургер
+function burgerMenu() {
+  const burger = document.querySelector('.burger')
+  const menu = document.querySelector('.menu')
+  const body = document.querySelector('body')
+  burger.addEventListener('click', () => {
+    if (!menu.classList.contains('active')) {
+      menu.classList.add('active')
+      burger.classList.add('active-burger')
+      body.classList.add('locked')
+    } else {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+  // Вот тут мы ставим брейкпоинт навбара
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 767.98) {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+}
+burgerMenu()
+
+
+// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
+function fixedNav() {
+  const nav = document.querySelector('nav')
+
+  // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
+  const breakpoint = 1
+  if (window.scrollY >= breakpoint) {
+    nav.classList.add('fixed__nav')
+  } else {
+    nav.classList.remove('fixed__nav')
+  }
+}
+window.addEventListener('scroll', fixedNav)
+
+
+// Аккордеон
+function accordion() {
+  const items = document.querySelectorAll('.accordion__item-trigger')
+  items.forEach(item => {
+      item.addEventListener('click', () => {
+          const parent = item.parentNode
+          if (parent.classList.contains('accordion__item-active')) {
+              parent.classList.remove('accordion__item-active')
+          } else {
+              document
+                  .querySelectorAll('.accordion__item')
+                  .forEach(child => child.classList.remove('accordion__item-active'))   
+              parent.classList.add('accordion__item-active')
+          }
+      })
+  })
+}
+accordion();
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "показать еще работы";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Показать меньше";
+    moreText.style.display = "inline";
+  }
+};
+
+// Модальное окно
+function bindModal(trigger, modal, close) {
+  trigger = document.querySelector(trigger),
+    modal = document.querySelector(modal),
+    close = document.querySelector(close)
+
+  const body = document.body
+
+  trigger.addEventListener('click', e => {
+    e.preventDefault()
+    modal.style.display = 'flex'
+    body.classList.add('locked')
+  });
+  close.addEventListener('click', () => {
+    modal.style.display = 'none'
+     body.classList.remove('locked')
+  });
+  modal.addEventListener('click', e => {
+    if (e.target === modal) {
+      modal.style.display = 'none'
+       body.classList.remove('locked')
+    }
+  })
+}
+
+// ПЕРВЫЙ аргумент - класс кнопки, при клике на которую будет открываться модальное окно.
+// ВТОРОЙ аргумент - класс самого модального окна.
+// ТРЕТИЙ аргумент - класс кнопки, при клике на которую будет закрываться модальное окно.
+bindModal('.application-modal__btn', '.application-modal__wrapper', '.application-modal__close')
+
+
+//filter
+
+var mixer = mixitup('#portfolio__filter, #portfolio__filter-content');
+// var mixer = mixitup('#portfolio__filter-content');
---
+++ src/js/scripts.js
@@ -0,0 +1,3 @@
+// Custom Scripts
+@@include('libs/mixitup.min.js')
+@@include('main.js')
